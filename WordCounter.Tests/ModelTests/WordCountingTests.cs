33e3ef6135cecfbc6b42be43547ad26d6e8a916a
Leilani Leach <leilanileach@yahoo.com>
2018-11-17 11:05:25 -0800

    fix foreach loop to add null and empty array conditions

--- WordCounter.Tests/ModelTests/WordCountingTests.cs
+++ WordCounter.Tests/ModelTests/WordCountingTests.cs
@@ -8,12 +8,17 @@ namespace WordCounter.Tests
   public class WordCountingTest
   {
     [TestMethod]
-    public void WordCount_WordCountConstructsObject_WordCount()
+    public void WordCounting_CreatesArrayOfWords_Array()
     {
         WordCounting testWordCount = new WordCounting("testQueryWord", "this is a test sentence");
         string[] testSentenceArray = {"this","is","a","test","sentence"};
         CollectionAssert.AreEqual(testSentenceArray, testWordCount.createSentenceArray(testWordCount.InputSentence));
-
+    }
+    [TestMethod]
+    public void WordCounting_FindsMatchingWord_True()
+    {
+        WordCounting testWordCount = new WordCounting("love", "I love C Sharp");
+        Assert.AreEqual(true, testWordCount.matchExists("love", new string[]{"I","love","C","Sharp"}));
     }
   }
 }
--- WordCounter/Models/WordCounting.cs
+++ WordCounter/Models/WordCounting.cs
@@ -3,24 +3,38 @@ using System.Collections.Generic;
 
 namespace WordCounter
 {
-  public class WordCounting
-  {
-    //   Class Definition
-      public string QueryWord;
-      public string InputSentence;
+    public class WordCounting
+    {
+        //   Class Definition
+        public string QueryWord;
+        public string InputSentence;
 
-    // Constructor
-      public WordCounting(string queryWord, string inputSentence)
-      {
-          QueryWord = queryWord;
-          InputSentence = inputSentence;
+        // Constructor
+        public WordCounting(string queryWord, string inputSentence)
+        {
+            QueryWord = queryWord;
+            InputSentence = inputSentence;
 
-      }
-      public string[] createSentenceArray(string userInput)
-      {
-        string[] sentenceArray = InputSentence.Split(' ');
-        // Console.WriteLine(sentenceArray);
-        return sentenceArray;
-      }
-  }
+        }
+        public string[] createSentenceArray(string userInput)
+        {
+            string[] sentenceArray = InputSentence.Split(' ');
+            // Console.WriteLine(sentenceArray);
+            return sentenceArray;
+        }
+        public bool matchExists(string userQueryWord, string[] sentenceArray)
+        {
+            if ((sentenceArray != null) & (sentenceArray.Length > 0))
+            {
+                foreach (string word in sentenceArray)
+                {
+                    if (word == userQueryWord)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false; 
+        }
+    }
 }
