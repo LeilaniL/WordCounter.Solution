84061c3f8f1ee2e115e6fdce30200ca64eb7f61e
Leilani Leach <leilanileach@yahoo.com>
2018-11-17 16:54:17 -0800

    add program to run in console

--- WordCounter/Models/WordCounting.cs
+++ WordCounter/Models/WordCounting.cs
@@ -34,10 +34,11 @@ namespace WordCounter
                     }
                 }
             }
-            return false; 
+            return false;
         }
-         public int matchCounter(string userQueryWord, string[] sentenceArray)
-        {int matchCount = 0;
+        public int matchCounter(string userQueryWord, string[] sentenceArray)
+        {
+            int matchCount = 0;
             if ((sentenceArray != null) & (sentenceArray.Length > 0))
             {
                 foreach (string word in sentenceArray)
@@ -49,7 +50,21 @@ namespace WordCounter
                 }
                 return matchCount;
             }
-            return 0; 
+            return 0;
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+          Console.WriteLine("Please enter a sentence.");
+          string sentence = Console.ReadLine();
+          Console.WriteLine("Please enter the word you'd like to count.");
+          string inputWord = Console.ReadLine();
+          WordCounting userInputStrings = new WordCounting(inputWord, sentence);
+          string[] userSentenceArray = userInputStrings.createSentenceArray(sentence);
+          int result = userInputStrings.matchCounter(inputWord, userSentenceArray);
+          Console.WriteLine(result);
         }
     }
 }
